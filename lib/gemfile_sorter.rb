16b441ac0dca92f32569cb58ae3ecf9c3e9f895f
Noel Rappin <noelrap@hey.com>
2023-11-26 08:19:58 -0600

    Upload with executable

--- lib/gemfile_sorter.rb
+++ lib/gemfile_sorter.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "fileutils"
 require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem
 loader.setup
@@ -9,6 +10,12 @@ module GemfileSorter
 
   def self.sort_gemfile(directory_name = ".")
     Parser.parse("#{directory_name}/Gemfile")
+  end
+
+  def self.execute(directory_name = ".")
+    result = Parser.parse("#{directory_name}/Gemfile")
+    FileUtils.mv("#{directory_name}/Gemfile", "#{directory_name}/Gemfile.unsorted")
+    File.write("#{directory_name}/Gemfile", result)
   end
 end
 
